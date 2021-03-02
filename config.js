47d69cd11d577529cbd7139982d200539cbaacd4
hironobu.ohara <hironobu.ohara@g.softbank.co.jp>
2021-03-02 14:37:32 +0900

    trying to publish

--- config.js
+++ config.js
@@ -1,25 +1,46 @@
 const config = {
   gatsby: {
     pathPrefix: '/',
-    siteUrl: 'https://www.sbcloud.co.jp/',
+    siteUrl: 'https://ohiro18.github.io/technical.site/',
     gaTrackingId: null,
     trailingSlash: false,
   },
   header: {
     logo: 'https://raw.githubusercontent.com/ohiro18/technical.site/master/src/components/images/TechnicalSite_1.png',
-    logoLink: 'https://www.sbcloud.co.jp/',
+    logoLink: 'https://ohiro18.github.io/technical.site/',
     title:
-      "<a href='https://www.sbcloud.co.jp/'><img class='img-responsive' src='https://raw.githubusercontent.com/ohiro18/technical.site/master/src/components/images/TechnicalSite_2.png' alt='Learn logo' /></a>",
+      "<a href='https://ohiro18.github.io/technical.site/'><img class='img-responsive' src='https://raw.githubusercontent.com/ohiro18/technical.site/master/src/components/images/TechnicalSite_2.png' alt='Learn logo' /></a>",
     githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
     helpUrl: '',
     tweetText: '',
     social: `<li>
 		    <a href="https://twitter.com/sbcloud_pr" target="_blank" rel="noopener">
 		      <div class="twitterBtn">
-		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
+		        <img src='https://raw.githubusercontent.com/ohiro18/technical.site/master/src/components/images/twitter-brands-block.svg' alt={'Twitter'}/>
 		      </div>
 		    </a>
-		  </li>
+		        </li>
+            <li>
+        <a href="https://www.facebook.com/SBCloud/" target="_blank" rel="noopener">
+          <div class="twitterBtn">
+            <img src='https://raw.githubusercontent.com/ohiro18/technical.site/master/src/components/images/facebook.svg' alt={'Twitter'}/>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.youtube.com/channel/UCLI7MTGFxKxjsEUH-oNvy8w" target="_blank" rel="noopener">
+          <div class="twitterBtn">
+            <img src='https://raw.githubusercontent.com/ohiro18/technical.site/master/src/components/images/youtube.svg' alt={'Twitter'}/>
+          </div>
+        </a>
+      </li>
+      <li>
+		    <a href="https://www.slideshare.net/sbcloud" target="_blank" rel="noopener">
+		      <div class="twitterBtn">
+		        <img src='https://raw.githubusercontent.com/ohiro18/technical.site/master/src/components/images/file-ppt.svg' alt={'Twitter'}/>
+		      </div>
+		    </a>
+		  </li>          
 			<li>
 		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
 		      <div class="discordBtn">
@@ -44,17 +65,17 @@ const config = {
     collapsedNav: [
       '/codeblock', // add trailing slash if enabled above
     ],
-    links: [{ text: 'Hasura', link: 'https://hasura.io' }],
+    links: [{ text: 'お問い合わせ', link: 'https://www.sbcloud.co.jp/contact_jp/' }],
     frontline: false,
     ignoreIndex: true,
-    title:
-      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
+    //title:
+    //  "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
   },
   siteMetadata: {
-    title: 'Gatsby Gitbook Boilerplate | Hasura',
-    description: 'Documentation built with mdx. Powering hasura.io/learn ',
+    title: 'SBクラウド - テクニカルリファレンス',
+    description: 'SBクラウドによるテクニカルリファレンスです',
     ogImage: null,
-    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
+    docsLocation: 'https://github.com/ohiro18/technical.site/tree/master/content',
     favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
   },
   pwa: {
