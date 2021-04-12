0bfaf782c7731774a05f701d94e2200a99d999f4
ohiro18 <hironobu.ohara@g.softbank.co.jp>
2021-04-12 19:36:02 +0900

    メタデータを変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,61 @@ const config = require("./config");
 
 const plugins = [
   'gatsby-plugin-sitemap',
+  `gatsby-transformer-remark`,
   'gatsby-plugin-sharp',
+  {
+    resolve: `gatsby-plugin-feed`,
+    options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+      feeds: [
+        {
+          serialize: ({ query: { site, allMarkdownRemark } }) => {
+            return allMarkdownRemark.edges.map(edge => {
+              return Object.assign({}, edge.node.frontmatter, {
+                description: edge.node.excerpt,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                custom_elements: [{ "content:encoded": edge.node.html }],
+              })
+            })
+          },
+          query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+                filter: {frontmatter: { draft: { ne: true } }}
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+        },
+      ],
+    },
+  },
   {
     resolve: `gatsby-plugin-layout`,
     options: {
