16e7ae6c014d619ba7362e8bf112bd945bd762f8
ohiro18 <hironobu.ohara@g.softbank.co.jp>
2021-04-20 15:35:22 +0900

    Mainページ変更

--- src/components/mdxComponents/index.js
+++ src/components/mdxComponents/index.js
@@ -10,25 +10,39 @@ const StyledPre = styled('pre')`
 `;
 
 export default {
+  // eslint-disable-next-line react/display-name
   h1: props => (
+    // eslint-disable-next-line jsx-a11y/heading-has-content
     <h1 className="heading1" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
+  // eslint-disable-next-line react/display-name
   h2: props => (
+    // eslint-disable-next-line jsx-a11y/heading-has-content
     <h2 className="heading2" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
+  // eslint-disable-next-line react/display-name
   h3: props => (
+    // eslint-disable-next-line jsx-a11y/heading-has-content
     <h3 className="heading3" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
+  // eslint-disable-next-line react/display-name
   h4: props => (
+    // eslint-disable-next-line jsx-a11y/heading-has-content
     <h4 className="heading4" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
+  // eslint-disable-next-line react/display-name
   h5: props => (
+    // eslint-disable-next-line jsx-a11y/heading-has-content
     <h5 className="heading5" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
+  // eslint-disable-next-line react/display-name
   h6: props => (
+    // eslint-disable-next-line jsx-a11y/heading-has-content
     <h6 className="heading6" id={props.children.replace(/\s+/g, '').toLowerCase()} {...props} />
   ),
+  // eslint-disable-next-line react/display-name
   p: props => <p className="paragraph" {...props} />,
+  // eslint-disable-next-line react/display-name
   pre: props => (
     <StyledPre>
       <pre {...props} />
